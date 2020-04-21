91ca7b43f8d5d6509a5004fe02eed479fd3f1d59
Jack Rose <jackalexanderrose@hotmail.com>
2020-04-21 12:47:42 +0100

    Initial Commit

---
+++ EscPosPrint/ESC-POS-USB-NET/Interfaces/Command/IBarCode.cs
@@ -0,0 +1,12 @@
+﻿using ESC_POS_USB_NET.Enums;
+
+namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    interface IBarCode
+    {
+        byte[] Code128(string code,Positions printString);
+        byte[] Code39(string code, Positions printString);
+        byte[] Ean13(string code, Positions printString);
+    }
+}
+
---
+++ EscPosPrint/ESC-POS-USB-NET/Interfaces/Command/IImage.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    internal interface IImage
+    {
+        /*
+        byte[] Print(Bitmap image);
+        */
+    }
+}
